96b289b53abdec32c4175ec3cd6944f647fdc649
Stefano Labianca <lstefano@protonmail.com>
2022-09-17 19:02:35 +0200

    scrittura dati su SD

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,33 @@
 #include <Wire.h>
 #include <BH1750.h>
 
+#include <FS.h>
+#include <SD.h>
+#include <SPI.h>
+
+void writeFile(File &file, float lux);
+void openFile(fs::FS &fs, const char* path);
+void writeFile(File &file);
+
 BH1750 lightMeter;
+File file;
+
+#define MAX_W 100
+int i = 0;
 
 void setup() 
 {
   	Serial.begin(9600);
 	Wire.begin();
+	
+	if (!SD.begin(5))
+	{
+		Serial.println("Scheda non montata correttamente");
+		return;
+	}
+
+	uint8_t cardType = SD.cardType();
+	Serial.printf("Tipo di scheda: %u\n", cardType);
 
 	if (!lightMeter.begin())
 	{
@@ -15,6 +36,7 @@ void setup()
 	}
 
 	Serial.println("Iniziano le misurazioni");
+	openFile(SD, "/lux_data.txt");
 }
 
 void loop() 
@@ -22,5 +44,50 @@ void loop()
 	float lux = lightMeter.readLightLevel();
 	Serial.printf("Lux level: %.2f\n", lux);
 
-  	delay(1000);
+	if (i == MAX_W)
+	{
+		file.close();
+		
+	}
+	
+	else
+	{
+		writeFile(file, lux);
+		writeFile(file);
+		
+		i++;
+		delay(500);
+	}
 }
+
+void openFile(fs::FS &fs, const char* path)
+{
+	file = fs.open(path, FILE_APPEND);
+	
+	if (!file)
+	{
+		Serial.println("Errore, file non esistente");
+		return;
+	}
+
+	Serial.println("File creato");
+}
+
+void writeFile(File &file, float lux)
+{
+	if (!file.print(lux))
+	{
+		Serial.println("Errore nella scrittura");
+		return;
+	}
+
+	file.print(lux);
+	Serial.println("Scrittura avvenuta con successo");
+}
+
+void writeFile(File &file)
+{
+	file.print("\n");
+}
+
+
