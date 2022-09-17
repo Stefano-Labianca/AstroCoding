f7e84b0fa97ad2b6d316411a8ae65e95ae6cf68b
Stefano Labianca <lstefano@protonmail.com>
2022-09-17 17:43:32 +0200

    misurazione della luce tramite sensore

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <BH1750.h>
 
+BH1750 lightMeter;
 
 void setup() 
 {
-  Serial.begin(9600);
+  	Serial.begin(9600);
+	Wire.begin();
+
+	if (!lightMeter.begin())
+	{
+		Serial.println("Errore nel sensore");	
+	}
+
+	Serial.println("Iniziano le misurazioni");
 }
 
 void loop() 
 {
-  Serial.println("AAAAAA"); 
-  delay(1000);
+	float lux = lightMeter.readLightLevel();
+	Serial.printf("Lux level: %.2f\n", lux);
+
+  	delay(1000);
 }
